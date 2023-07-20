0d7a528c6a7e5e411a7d250f190c0d8617ea79d3
Onup Chandra Barmon <43741354+onuproy@users.noreply.github.com>
2023-07-21 03:02:33 +0600

    Update main function

--- train.php
+++ train.php
@@ -3,3 +3,35 @@
 #include <GL/gl.h>
 #include <stdlib.h>
 #define SPEED 30.0
+
+
+float i=0.0,m=0.0,n=0.0,o=0.0,c=0.0;
+
+int light=1,day=1,plane=0,comet=0,xm=900,train=0;
+char ch;
+
+void declare(char *string)
+{
+     while(*string)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *string++);
+}
+
+void draw_pixel(GLint cx, GLint cy)
+{
+
+	glBegin(GL_POINTS);
+		glVertex2i(cx,cy);
+	glEnd();
+}
+
+void plotpixels(GLint h,GLint k, GLint x,GLint y)
+{
+	draw_pixel(x+h,y+k);
+	draw_pixel(-x+h,y+k);
+	draw_pixel(x+h,-y+k);
+	draw_pixel(-x+h,-y+k);
+	draw_pixel(y+h,x+k);
+	draw_pixel(-y+h,x+k);
+	draw_pixel(y+h,-x+k);
+	draw_pixel(-y+h,-x+k);
+}
